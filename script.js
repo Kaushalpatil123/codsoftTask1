2bd59f58d2fc507d8534ba5e0b3652621d65f3f2
kaushal patil <91415972+Kaushalpatil123@users.noreply.github.com>
2023-07-08 17:15:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -102,5 +102,5 @@ function buttonClick6() {
 
 function completesignin() {
   alert("Login Successful");
-  window.location.href = "http://127.0.0.1:5500/index.html";
+  window.location.href = "https://kaushalpatil123.github.io/codsoftTask1/";
 }
